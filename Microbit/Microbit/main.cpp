f2ddd5d922cb0027466028979d075bc66c99a338
Jávorszky Mazsi <javorszky.mariann@gmail.com>
2017-02-22 10:59:33 +0100

    refactor

--- Microbit/Microbit/main.cpp
+++ Microbit/Microbit/main.cpp
@@ -21,8 +21,8 @@ int main(int argc, char** argv) {
 int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
-  //ServiceManager myServiceManager(&a);
-  MainWindow w(&a);
+  ServiceManager myServiceManager(&a);
+  MainWindow w;
   w.show();
 
   return a.exec();
